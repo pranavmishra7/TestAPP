83cc1fc5a5c80b3cf738fd3d413db0b07ed67913
Pranav Mishra <pranav.mishra@hotmail.com>
2022-05-22 14:46:25 +0530

    added initial changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormComponent } from './form/form.component';
+import { NgMaterialModule } from './ng-material/ng-material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgMaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
